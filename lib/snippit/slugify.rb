bf2fadeba795e7fc59f738c2064b94f19f399aba
Spenser Black <spenserblack01@gmail.com>
2022-12-14 09:08:24 -0500

    Improve doc comment for `slugify`

--- lib/snippit/slugify.rb
+++ lib/snippit/slugify.rb
@@ -4,6 +4,9 @@ class Snippit
   # Provides the slugify function
   module Slugify
     # Slugify a string
+    #
+    # @param [String] str The string to slugify
+    # @return [String] The slugified string
     def slugify(str)
       str.strip.downcase.gsub(/ /, '-').gsub(/[^a-z0-9\-_.]/, '')
     end
