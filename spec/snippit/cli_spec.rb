6199959f5b2cfdc4e5dfb9ef23e8b32c0ae36800
Spenser Black <spenserblack01@gmail.com>
2023-01-27 13:47:13 +0000

    Test saving when snippet directory doesn't exist

--- spec/snippit/cli_spec.rb
+++ spec/snippit/cli_spec.rb
@@ -27,4 +27,30 @@ RSpec.describe Snippit::CLI do
       expect(described_class.new(args).start).to eq(1)
     end
   end
+
+  context 'when `--save "My Snippet"` is given' do
+    let(:args) { ['--save', 'My Snippet'] }
+
+    before do
+      allow(File).to receive(:write)
+      allow(FileUtils).to receive(:mkdir_p)
+    end
+
+    context 'when the .snippit directory does not exist' do
+      before do
+        allow(File).to receive(:exist?).and_return(false)
+        allow(File).to receive(:read).with('My Snippet').and_return('foo')
+      end
+
+      it 'creates the ~/.snippit directory' do
+        described_class.new(args).start
+        expect(FileUtils).to have_received(:mkdir_p).with(File.expand_path('.snippit', Dir.home)).at_least(:once)
+      end
+
+      it 'writes the snippet to the ~/.snippit directory' do
+        described_class.new(args).start
+        expect(File).to have_received(:write).with(File.expand_path('.snippit/my-snippet', Dir.home), 'foo')
+      end
+    end
+  end
 end
